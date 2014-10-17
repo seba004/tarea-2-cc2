49d448a5bbf66d4372a9f7a919bf244bc86fad32
seba004 <sebastian.tapia@alumnos.usm.cl>
2014-10-17 00:18:27 -0300

    funciones a full

--- funciones.py
+++ funciones.py
@@ -13,14 +13,14 @@ from  numpy import  *
 from scipy.misc import derivative
 import numpy as np
 from scipy import *
-from matplotlib import *
-import matplotlib.pyplot as plt
+#from matplotlib import *
+#import matplotlib.pyplot as plt
 from numpy.linalg import norm, solve
 from scipy import linalg as al#algebra lineal
 
-def midpoint (intervalos, funcion):
-    h=(float(1)-float(0))/float(intervalos)
-    valor=0
+def midpoint (intervalos, funcion,a,b):#(a,b) es de donde a donde va la int
+    h=(float(b)-float(a))/float(intervalos)
+    valor=a
     suma=0
     for i in range(1,intervalos+1):
         wi=(valor+valor+h)/2
@@ -28,12 +28,12 @@ def midpoint (intervalos, funcion):
         valor=valor+h
     return suma*h
 
-def trapezoide(intervalos,funcion):
-    h=(float(1)-float(0))/float(intervalos)
+def trapezoide(intervalos,funcion,a,b):#(a,b) es de donde a donde va la int
+    h=(float(b)-float(a))/float(intervalos)
     h2=h/2
-    fa=funcion(0)
-    fb=funcion(1)
-    numero=0
+    fa=funcion(a)
+    fb=funcion(b)
+    numero=a
     suma=0
     for i in range(1,intervalos):
         numero=numero+h
@@ -42,13 +42,13 @@ def trapezoide(intervalos,funcion):
     suma=suma*2
     final=(fa+fb+suma)*h2
     return final
-
-def simpson (intervalos,funcion):
-    h=(float(1)-float(0))/float(intervalos)
+#creo que interbalos tiene queser pares para simpson
+def simpson (intervalos,funcion,a,b):#(a,b) es de donde a donde va la int
+    h=(float(b)-float(a))/float(intervalos)
     h3=h/3
-    fa=funcion(0)
-    fb=funcion(1)
-    x=np.linspace(0,1,intervalos+1)
+    fa=funcion(a)
+    fb=funcion(b)
+    x=np.linspace(a,b,intervalos+1)
     suma1=0
     for i in range(1,intervalos/2):
         suma1= suma1+funcion(x[2*i])
@@ -61,17 +61,27 @@ def simpson (intervalos,funcion):
     return final
 
 
-
+def gauss (intervalos,funcion):
+    
+    xi,wi=np.polynomial.legendre.leggauss(intervalos)
+    suma=0
+    
+    for i in range (1,intervalos):
+        suma=suma+(wi[i]*funcion(xi[i]))
+    suma=suma+wi[0]*funcion(xi[0])
+    return suma
+    
 
 
 if __name__ == '__main__':
     myfun = lambda x : x**2 #1 # x #np.exp(-x)
-    intervalos=6
-    a= midpoint(intervalos,myfun)
-    b= trapezoide(intervalos,myfun)
-    c= simpson(intervalos,myfun)
+    intervalos=
+    a= midpoint(intervalos,myfun,-1,1)
+    b= trapezoide(intervalos,myfun,-1,1)
+    c= simpson(intervalos,myfun,-1,1)
+    d=gauss(intervalos,myfun)
     print("midpoint es: ",a)
     print("trapecio es: ",b)
     print("simpson es: ",c)
-
+    print("gauss es: ",d)
 
