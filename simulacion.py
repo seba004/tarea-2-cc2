0b70b6c31fa18a407133d3585c48d1ce6ff574e8
unknown <sebastian.tapia@alumnos.usm.cl>
2014-10-20 03:51:46 -0300

    se obtinee influencia total x e y sobre cada agente

--- simulacion.py
+++ simulacion.py
@@ -9,6 +9,87 @@ from numpy.linalg import norm, solve
 from scipy import linalg as al#algebra lineal
 import time
 
+
+
+class agente:
+    def __init__(self,x,y,peso,vx,vy,influencia_x,influencia_y):
+        self.x=x
+        self.y=y
+        self.peso=peso
+        self.vx=vx
+        self.vy=vy
+        self.influencia_x=influencia_x
+        self.influencia_y=influencia_y
+
+    def change_influ_x(self,influ_in_x):
+        self.influencia_x=influ_in_x
+
+    def change_influ_y(self,influ_in_y):
+        self.influencia_y=influ_in_y
+
+    def move_x(self,x_in):
+        self.x=x_in
+
+    def move_y(self,y_in):
+        self.y=y_in
+
+    def move_vx(self,vx_in):
+        self.vx=vx_in
+
+    def move_vy(self,vy_in):
+        self.vy=vy_in
+
+def influencia(agente1,agente2,ri):
+    distancia_x=abs(agente1.x -agente2.x)
+    distancia_y=abs(agente1.y -agente2.y)
+    vector_x=distancia_x/sqrt(distancia_x**2 +distancia_y**2)
+    vector_y=distancia_y/sqrt(distancia_x**2 +distancia_y**2)
+    e=math.e
+    influencia_x=0
+    influencia_y=0
+    if (distancia_x < ri):
+        influencia_x= e*((float(agente1.peso)*float(agente2.peso))/float(ri*distancia_x))*float(vector_x)
+    if (distancia_x >= ri):
+        influencia_x= ((e*float(agente1.peso)*float(agente2.peso))/float(distancia_x**2))*-float(vector_x)
+
+    if (distancia_y < ri):
+        influencia_y= e*((float(agente1.peso)*float(agente2.peso))/float(ri*distancia_y))*float(vector_y)
+    if (distancia_y >= ri):
+        influencia_y= ((e*float(agente1.peso)*float(agente2.peso))/float(distancia_y**2))*-float(vector_y)
+
+    return influencia_x,influencia_y
+
+def sum_influencias(lista_agentes,ri):
+    cantidad=len(lista_agentes)
+    for i in range (cantidad):
+        suma_x=0
+        suma_y=0
+        for j in range(1,cantidad):
+            valor_x,valor_y=influencia(lista_agentes[0],lista_agentes[j],ri)
+            suma_x=suma_x+valor_x
+            suma_y=suma_y+valor_y
+        lista_agentes[0].change_influ_x(suma_x)
+        lista_agentes[0].change_influ_y(suma_y)
+        primer = lista_agentes.pop( 0 )
+        lista_agentes.append(primer)
+    return lista_agentes
+
+def ingreso_datos():
+    ri=45 # rango maximo de accion de repulsion
+    pi= 2 # peso de agentes
+    lista_agentes=[]
+    tiempo= 45 #tiempo de simulacion
+    lista_agentes.append(agente(0,0,pi,5,5,0,0))# xi,yi,pi,vx,vy,influenciax,influencia y donde x e y son pos y vx y vy son vel
+    lista_agentes.append(agente(2,2,pi,5,5,0,0))# xi,yi,pi,vx,vy,influenciax,influencia y donde x e y son pos y vx y vy son vel
+    lista_agentes.append(agente(5,5,pi,5,5,0,0))# xi,yi,pi,vx,vy,influenciax,influencia y donde x e y son pos y vx y vy son vel
+    lista_agentes=sum_influencias(lista_agentes,ri)
+
+    return lista_agentes
+
+
+
+
+#funciones dibujooooo
 def crear_matriz(fila,columna):
     matriz= np.zeros([fila,columna],dtype='int')
     return matriz
@@ -16,17 +97,6 @@ def crear_matriz(fila,columna):
 def imprimir_matriz(matriz):
     print '\n'.join(' '.join(str(cell) for cell in row) for row in matriz)
     return
-
-class agente:
-    def __init__(self,x,y,peso):
-        self.x=x
-        self.y=y
-        self.peso=peso
-    def move_x(self,x_in):
-        self.x=x_in
-    def move_y(self,y_in):
-        self.y=y_in
-
 
 def cantidad_agentes():
     print("Cuantos agentes desea?")
@@ -53,9 +123,10 @@ def incorporacion_agentes(agentes,matriz):
 
 
 if __name__ == '__main__':
-    a=crear_matriz(15,15)
-    imprimir_matriz(a)
-    b=cantidad_agentes()
-    t=creacion_agentes(b)
-    ma=incorporacion_agentes(t,a)
-    imprimir_matriz(ma)
+    #a=crear_matriz(15,15)
+    #imprimir_matriz(a)
+    #b=cantidad_agentes()
+    #t=creacion_agentes(b)
+    #ma=incorporacion_agentes(t,a)
+    #imprimir_matriz(ma)
+    t=ingreso_datos()
