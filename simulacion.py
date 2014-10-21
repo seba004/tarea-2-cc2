b9e70e5a866cb8e6afc26174d733e7f300b75c51
unknown <sebastian.tapia@alumnos.usm.cl>
2014-10-20 23:15:48 -0300

    simulacion ok

--- simulacion.py
+++ simulacion.py
@@ -42,19 +42,26 @@ class agente:
 def influencia(agente1,agente2,ri):
     distancia_x=abs(agente1.x -agente2.x)
     distancia_y=abs(agente1.y -agente2.y)
+    print(distancia_x)
     vector_x=distancia_x/sqrt(distancia_x**2 +distancia_y**2)
     vector_y=distancia_y/sqrt(distancia_x**2 +distancia_y**2)
     e=math.e
+
     influencia_x=0
     influencia_y=0
-    if (distancia_x < ri):
+    if (distancia_x < ri and distancia_x != float(0)):
+
         influencia_x= e*((float(agente1.peso)*float(agente2.peso))/float(ri*distancia_x))*float(vector_x)
-    if (distancia_x >= ri):
+    if (distancia_x >= ri and distancia_x != float(0)):
+
         influencia_x= ((e*float(agente1.peso)*float(agente2.peso))/float(distancia_x**2))*-float(vector_x)
 
-    if (distancia_y < ri):
+
+
+
+    if (distancia_y < ri and distancia_y != 0):
         influencia_y= e*((float(agente1.peso)*float(agente2.peso))/float(ri*distancia_y))*float(vector_y)
-    if (distancia_y >= ri):
+    if (distancia_y >= ri and distancia_y != 0):
         influencia_y= ((e*float(agente1.peso)*float(agente2.peso))/float(distancia_y**2))*-float(vector_y)
 
     return influencia_x,influencia_y
@@ -66,6 +73,7 @@ def sum_influencias(lista_agentes,ri):
         suma_y=0
         for j in range(1,cantidad):
             valor_x,valor_y=influencia(lista_agentes[0],lista_agentes[j],ri)
+            #print(valor_x)
             suma_x=suma_x+valor_x
             suma_y=suma_y+valor_y
         lista_agentes[0].change_influ_x(suma_x)
@@ -75,13 +83,26 @@ def sum_influencias(lista_agentes,ri):
     return lista_agentes
 
 def ingreso_datos():
-    ri=45 # rango maximo de accion de repulsion
-    pi= 2 # peso de agentes
+    ri=10 # rango maximo de accion de repulsion
+    pi= 5 # peso de agentes
     lista_agentes=[]
     tiempo= 45 #tiempo de simulacion
-    lista_agentes.append(agente(2,1,pi,5,5,0,0))# xi,yi,pi,vx,vy,influenciax,influencia y donde x e y son pos y vx y vy son vel
-    lista_agentes.append(agente(1,5,pi,5,5,0,0))# xi,yi,pi,vx,vy,influenciax,influencia y donde x e y son pos y vx y vy son vel
-    lista_agentes.append(agente(4,8,pi,5,5,0,0))# xi,yi,pi,vx,vy,influenciax,influencia y donde x e y son pos y vx y vy son vel
+#    lista_agentes.append(agente(2,2,pi,0,0,0,0))# xi,yi,pi,vx,vy,influenciax,influencia y donde x e y son pos y vx y vy son vel
+#    lista_agentes.append(agente(-2,2,pi,0,0,0,0))# xi,yi,pi,vx,vy,influenciax,influencia y donde x e y son pos y vx y vy son vel
+#    lista_agentes.append(agente(-2,-2,pi,0,0,0,0))# xi,yi,pi,vx,vy,influenciax,influencia y donde x e y son pos y vx y vy son vel
+#    lista_agentes.append(agente(2,-2,pi,0,0,0,0))# xi,yi,pi,vx,vy,influenciax,influencia y donde x e y son pos y vx y vy son vel
+#    lista_agentes.append(agente(0,0,pi,0,0,0,0))# xi,yi,pi,vx,vy,influenciax,influencia y donde x e y son pos y vx y vy son vel
+    lista_agentes.append(agente(0,0,1,0,0,0,0))# xi,yi,pi,vx,vy,influenciax,influencia y donde x e y son pos y vx y vy son vel
+    lista_agentes.append(agente(4,1,pi,0,0,0,0))# xi,yi,pi,vx,vy,influenciax,influencia y donde x e y son pos y vx y vy son vel
+    lista_agentes.append(agente(4,3,pi,0,0,0,0))# xi,yi,pi,vx,vy,influenciax,influencia y donde x e y son pos y vx y vy son vel
+    lista_agentes.append(agente(1,4,pi,0,0,0,0))# xi,yi,pi,vx,vy,influenciax,influencia y donde x e y son pos y vx y vy son vel
+    lista_agentes.append(agente(-3,1,pi,0,0,0,0))# xi,yi,pi,vx,vy,influenciax,influencia y donde x e y son pos y vx y vy son vel
+    lista_agentes.append(agente(-3,4,pi,0,0,0,0))# xi,yi,pi,vx,vy,influenciax,influencia y donde x e y son pos y vx y vy son vel
+    lista_agentes.append(agente(2,-1,pi,0,0,0,0))# xi,yi,pi,vx,vy,influenciax,influencia y donde x e y son pos y vx y vy son vel
+    lista_agentes.append(agente(5,-3,pi,0,0,0,0))# xi,yi,pi,vx,vy,influenciax,influencia y donde x e y son pos y vx y vy son vel
+    lista_agentes.append(agente(-4,-1,pi,0,0,0,0))# xi,yi,pi,vx,vy,influenciax,influencia y donde x e y son pos y vx y vy son vel
+    lista_agentes.append(agente(-2,-2,pi,0,0,0,0))# xi,yi,pi,vx,vy,influenciax,influencia y donde x e y son pos y vx y vy son vel
+    lista_agentes.append(agente(-2,-4,pi,0,0,0,0))# xi,yi,pi,vx,vy,influenciax,influencia y donde x e y son pos y vx y vy son vel
     lista_agentes=sum_influencias(lista_agentes,ri)
 
     return lista_agentes
@@ -98,46 +119,25 @@ def edo_euler(funcion, v_inicial, n):
 def aplicacion_euler(lista_agentes):
     cantidad=len(lista_agentes)
     for i in range (cantidad):
+        #print(lista_agentes[i].influencia_x)
         valor_x=round(edo_euler(lista_agentes[i].influencia_x,lista_agentes[i].vx,100))
         valor_y=round(edo_euler(lista_agentes[i].influencia_y,lista_agentes[i].vy,100))
         base_x=lista_agentes[i].x
         base_y=lista_agentes[i].y
+        if(base_x <0 and base_y>0):
+            valor_x=abs(valor_x)
+        if(base_x<0 and base_y<0):
+            valor_y=abs(valor_y)
+            valor_x=abs(valor_x)
+        if(base_x>0 and base_y<0):
+            valor_y=abs(valor_y)
+
         lista_agentes[i].move_x(base_x+valor_x)
         lista_agentes[i].move_y(base_y+valor_y)
     return lista_agentes
 
 
 #funciones dibujooooo
-def crear_matriz(fila,columna):
-    matriz= np.zeros([fila,columna],dtype='int')
-    return matriz
-
-def imprimir_matriz(matriz):
-    print '\n'.join(' '.join(str(cell) for cell in row) for row in matriz)
-    return
-
-def cantidad_agentes():
-    print("Cuantos agentes desea?")
-    agentes = input()
-    return agentes
-
-def creacion_agentes(cantidad):
-    agentes=[]
-    for i in range(cantidad):
-        print("posicion en x del agente ",i)
-        pos_x = input()
-        print("posicion en y del agente ",i)
-        pos_y = input()
-        print("peso del agente ",i)
-        peso = input()
-        i = agente(pos_x,pos_y,peso)
-        agentes.append(i)
-    return agentes#lista de agentes
-
-def incorporacion_agentes(agentes,matriz):
-    for i in agentes:
-        matriz[i.x,i.y]=1
-    return matriz
 
 def listas (lista_agentes):
     cantidad=len(lista_agentes)
@@ -155,21 +155,23 @@ def simulacion ():
     agentes=ingreso_datos()
     x,y=listas(agentes)
     points, = ax.plot(x, y, marker='o', linestyle='None')
-    ax.set_xlim(0, 50)
-    ax.set_ylim(0, 50)
-    tim=4
+    ax.set_xlim(-120, 120)
+    ax.set_ylim(-120, 120)
+    tim=10
     t1= time.clock()
     while True:
         agentes=aplicacion_euler(agentes)
         x,y=listas(agentes)
         points.set_data(x, y)
-        plt.pause(0.25)
+        plt.pause(0.5)
         t2= time.clock()
         if (t2-t1 >= tim):
+            plt.close()
             break
-    return
+    return agentes
+
 
 
 if __name__ == '__main__':
 
-    simulacion()
+    t= simulacion()
